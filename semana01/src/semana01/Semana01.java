4a0c9ee8e0941c1b45a8d123e28bab5667c5c9c2
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-05-30 09:58:19 -0600

    commit 300520232512

---
+++ semana01/src/semana01/Semana01.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana01;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class Semana01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //imprime texto en la consola de sistema y hace una nueva linea
+        System.out.println("Hola UNAH");
+        System.out.println("Ingenieria en Sistemas");
+        //imprime texto en la consola de sistema y no hace nueva linea
+        System.out.print("Soy Gerardo Portillo");
+        System.out.print("Vivo en SPS");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("Texto");
+        System.out.print("Soy Gerardo\nVivo en SPS\n");
+        System.out.println("Num\tNombre\tEdad");
+        System.out.println("1\tJosue\t10");
+        System.out.println("2\tViena\t14");
+        System.out.println("3\tIrene\t40");
+        System.out.println("");
+        //declaracion de variables
+        double x = 10.0/3.0;
+        System.out.println(x);
+        float y = 10.0f/3.0f;
+        System.out.println(y);
+        System.out.println(Math.PI);
+    }
+    
+}
