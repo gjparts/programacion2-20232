44d095e44f7dad19195e3d9b6c7b370f9f307460
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-07-13 11:02:25 -0600

    commit 1307202310425

--- string09/src/string09/String09.java
+++ string09/src/string09/String09.java
@@ -1,5 +1,5 @@
 package string09;
-
+import java.util.Scanner;
 public class String09 {
     public static void main(String[] args) {
         
@@ -19,9 +19,38 @@ public class String09 {
         System.out.println("Las siguientes comprobaciones son ignorando mayusc/minusc.");
         System.out.println( "Contiene MANgo: "+s1.toLowerCase().contains("MANgo".toLowerCase()) );
         
-        //preguntar al usuario que desea buscar dentro de s1 ignorando mayusculas/misnuculas
-        //si lo que el usuario desea buscar se encuentra entonces responder: "Texto encontrado"
+        //preguntar al usuario: ¿que desea buscar dentro de s1?
+        //ignorando mayusculas/misnuculas
+        //si lo que el usuario desea buscar se encuentra entonces
+        //responder: "Texto encontrado"
         //del lo contrario responder: "Texto no se encontró"
+        Scanner s = new Scanner(System.in);
         
+        System.out.println(s1);
+        System.out.print("Digitar lo que desea buscar: ");
+        String buscar = s.nextLine();
+        
+        if( s1.toUpperCase().contains( buscar.toUpperCase() ) == true )
+            System.out.println("Texto encontrado");
+        else
+            System.out.println("Texto no se encontró");
+        
+        //preguntar un numero de cedula, si el valor digitado tiene
+        //güiones entonces decir que no es valido del contrario
+        //decir que esta correcto.
+        System.out.print("Digitar numero de identidad (sin güiones): ");
+        String cedula = s.nextLine();
+        
+        //validar la cadena
+        if( cedula.contains("-") == true )
+            System.out.println("Cedula no debe tener güiones.");
+        else
+            System.out.println("Cedula correcta.");
     }   
 }
+
+
+
+
+
+
---
+++ string10/src/string10/String10.java
@@ -0,0 +1,26 @@
+package string10;
+public class String10 {
+    public static void main(String[] args) {
+        //SUBSTRING: extrae texto contenido en otro String.
+        //se debe proporcionar posicion desde y posicion hasta.
+        //recuerde que los String se numeran de 0 a N-1
+        /*Leer un numero de cedula de Honduras, el programa
+        unicamente debe validar que este numero tenga 13 caracteres.
+        *Luego de leer la cedula mostrar la informacion siguiente:
+        1) numero de departamento
+        2) numero de municipio
+        3) año de nacimiento
+        4) correlativo en que fue asentado
+        * el programa deberá eliminar los espacios de relleno, güiones,
+          güiones bajos, espacios en blanco de la cadena.
+        ejemplo:
+        Digitar cedula: 1401-1982-01055
+        Resultado:
+        Departamento: 14
+        Municipio: 01
+        Año nacimiento: 1982
+        Correlativo: 01055
+        */
+    }
+    
+}
